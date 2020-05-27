ec3ba959da633e2d836d87df301c7e2d454839a2
ssstoyanov <46551118+ssstoyanov@users.noreply.github.com>
2020-05-27 21:10:33 +0300

    todo MessageService.c

--- source/Services/MessageService.c
+++ source/Services/MessageService.c
@@ -4,8 +4,14 @@
 
 #include "MessageService.h"
 #include "TimeService.h"
+#include "stdbool.h"
 
 #define PORT 8080
+
+/** TODO
+ * добавить авторизацию и регистрацию
+ * добавить обработку нажатия клавиши для выхода из чата
+ */
 
 char *username = "User";
 
@@ -13,37 +19,44 @@ void sendingMessage(SOCKET clientSocket) {
 
     char message[1024];
 
-    printf("%s: ", username);
-    scanf("%s", message);
+    /*
+    printf("Enter your username: ");
+    scanf("&s\n", username);
+    */
 
-    sprintf("[%s] %s: %s", getCurrentTime(), username, message);
-    int ret = send(clientSocket, message, strlen(message), 0);
+    while (true) {
+        char text[512];
+        scanf("%s", text);
 
-    if (ret == SOCKET_ERROR) {
-        printf("Can't send message\n");
-        closesocket(clientSocket);
-        return;
-    }
+        sprintf(message, "[%s] %s: %s", getCurrentTime(), username, text);
+        int ret = send(clientSocket, message, strlen(message), 0);
 
-    printf("[%s] Sent: %s, includes %d bytes\n", getCurrentTime(), message, ret);
-    ret = SOCKET_ERROR;
-
-    while (ret == SOCKET_ERROR) {
-        //полчение ответа
-        ret = recv(clientSocket, message, 1024, 0);
-        //обработка ошибок
-        if (ret == 0 || ret == WSAECONNRESET) {
-            printf("[%s] Connection closed\n", getCurrentTime());
-            break;
+        if (ret == SOCKET_ERROR) {
+            printf("Can't send message\n");
+            closesocket(clientSocket);
+            return;
         }
-        if (ret < 0) {
-            //printf("Can't resieve message\n");
-            /*closesocket(clientSocket);
-            return;*/
-            continue;
+
+        printf("[%s] Sent: %s, includes %d bytes\n", getCurrentTime(), message, ret);
+        ret = SOCKET_ERROR;
+
+        while (ret == SOCKET_ERROR) {
+            //полчение ответа
+            ret = recv(clientSocket, message, 1024, 0);
+            //обработка ошибок
+            if (ret == 0 || ret == WSAECONNRESET) {
+                printf("[%s] Connection closed\n", getCurrentTime());
+                break;
+            }
+            if (ret < 0) {
+                //printf("Can't resieve message\n");
+                /*closesocket(clientSocket);
+                return;*/
+                continue;
+            }
+            //вывод на экран количества полученных байт и сообщение
+            printf("[%s] Receive: %s, includes %d bytes\n", getCurrentTime(), message, ret);
         }
-        //вывод на экран количества полученных байт и сообщение
-        printf("[%s] Receive: %s, includes %d bytes\n", getCurrentTime(), message, ret);
     }
 
 }
