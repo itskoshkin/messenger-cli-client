8b0ea83807f152402547e6a97a26834008de2eb8
ssstoyanov <46551118+ssstoyanov@users.noreply.github.com>
2020-06-01 19:53:24 +0300

    minor changes in reformat code and variables

--- source/Services/ConnectService.c
+++ source/Services/ConnectService.c
@@ -12,7 +12,7 @@ void connectToServer(SOCKET * clientSocket) {
     *clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
 
     if (*clientSocket == INVALID_SOCKET) {
-        printf("[%s] Error create socket\n", getCurrentTime());
+        printf("[%s] ERROR: Error create socket\n", getCurrentTime());
         return;
     }
 
@@ -22,7 +22,7 @@ void connectToServer(SOCKET * clientSocket) {
     serverSockaddr_in.sin_addr.S_un.S_addr = inet_addr("127.0.0.1"); //special look-up address
 
     if (connect(*clientSocket, (struct sockaddr *) &serverSockaddr_in, sizeof(serverSockaddr_in)) == SOCKET_ERROR) {
-        printf("[%s] Can't connect to server\n", getCurrentTime());
+        printf("[%s] ERROR: Can't connect to server\n", getCurrentTime());
         closesocket(*clientSocket);
         exit(EXIT_FAILURE);
     }
